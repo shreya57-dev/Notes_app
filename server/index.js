2c8854cf17e4f27ff25ee0b55e6cb15bf104710d
Shreya <shreyatripathi157@gmail.com>
2026-06-07 17:27:26 +0530

    setup backend and database connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from "cors";
+import pool from "./db.js"
 
 const app=express();
 
 app.use(cors());//to enable cors so that react app can communicate with this server 
 app.use(express.json());//parse incomming json requests and puts the data inside req.body
+
+app.get("/",(req,res)=>{
+    res.send("backend is working!");
+});
 
 
 app.listen(5000,()=>{
