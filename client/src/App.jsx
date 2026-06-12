171fbbcc268f3bf8835d13ad553b59840e74c723
Shreya <shreyatripathi157@gmail.com>
2026-06-12 10:53:18 +0530

    Implement notes frontend fetch and create note features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,122 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [notes,setNotes]=useState([]);
+  const [title,setTitle]=useState("");
+  const [content,setContent]=useState("");
+
+  const fetchNotes = async()=>{
+    const response=await axios.get("http://localhost:5000/notes");
+    setNotes(response.data);
+    console.log(response.data);
+  };
+
+  const addNote= async()=>{
+    try{
+    await axios.post("http://localhost:5000/notes",
+      {title,
+       content
+      }
+    );
+    fetchNotes();
+    setTitle("");
+    setContent("");
+  }
+  catch(err){
+    console.error(err);
+  }
+  };
+
+  useEffect(()=>{
+    fetchNotes();
+  },[]);
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <h1>Notes App</h1>
 
-      <div className="ticks"></div>
+ <input 
+      type="text" 
+      placeholder="Enter title"
+      value={title}
+      onChange={(e)=>setTitle(e.target.value)}
+      />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <textarea
+      placeholder="Enter your note content"
+      value={content}
+      onChange={(e)=>setContent(e.target.value)}
+      />
+      <button onClick={addNote}>Add note</button>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <p>Title: {title}</p>
+      <p>Content: {content}</p>
+
+      {notes.map((note)=>(
+        <div key={note.id}>
+          <h1>{note.title}</h1>
+          <p>{note.content}</p>
+        </div>
+      ))}
+
+     
     </>
-  )
-}
+  );
+}// coyyyduuuuuu deeeaaarrrrrr lalalalalalalala o myyyy dumboodaaaaaaaaaaaaa o my daaaaaaaaarlingggggggggggg
 
-export default App
+export default App;
+
